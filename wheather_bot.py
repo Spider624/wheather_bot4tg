cca59fd24be559ed7a8e7343f20a6297e3b67bb3
Clouise <spider624@mail.ru>
2023-06-04 21:02:53 +0400

    4.0 add air pollution api and markdown

--- wheather_bot.py
+++ wheather_bot.py
@@ -27,7 +27,9 @@ async def send_startup_message():
 async def get_weather(message: types.Message):
     try:
         city = message.text.lower()
+        
         response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={config.weather_api_token}")
+
         data = response.json()
         city = data["name"]
         cur_temp = data["main"]["temp"]
@@ -43,6 +45,15 @@ async def get_weather(message: types.Message):
         sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
         sunrise = sunrise_timestamp - delta_time
         sunset = sunset_timestamp - delta_time
+        
+        lat = data["coord"]["lat"]
+        lon = data["coord"]["lon"]
+        response_pollution = requests.get(f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={config.weather_api_token}")
+        data_pollution = response_pollution.json()
+        
+        #Air Quality Index. Possible values: 1, 2, 3, 4, 5. Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.
+        air_pollution_index = data_pollution["list"][0]["main"]["aqi"]
+        air_pollution_co = data_pollution["list"][0]["components"]["co"]
 
         # продолжительность дня
         length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) -       datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
@@ -68,11 +79,10 @@ async def get_weather(message: types.Message):
 
         zero_datetime = datetime.datetime(1970, 1, 1, 4, 0, 0)
         message_reply = (
-            f"{(datetime.datetime.now() - delta_time).strftime('%Y-%m-%d')}\n"
-            f"Локальное время: {(datetime.datetime.now() - delta_time).strftime('%H:%M')}\n"
-            f"Погода в городе: {city}\nТемпература: {cur_temp}°C {wd}\n"
-            f"Ощущается как: {round(feels_like_temp)}°C\n"
-            f"Влажность: {humidity}%\nДавление: {math.ceil(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с \n"
+            f"Локальное время: {(datetime.datetime.now() - delta_time).strftime('*%H:%M* %d-%m-%Y')}\n"
+            f"Погода в городе: {city}\nТемпература: *{cur_temp}°C* {wd}\n"
+            f"Ощущается как: *{round(feels_like_temp)}°C*\n"
+            f"Влажность: *{humidity}%*\nДавление: *{math.ceil(pressure/1.333)} мм.рт.ст*\nВетер: *{wind} м/с* \n"
         )
         if sunrise_timestamp > zero_datetime:
             message_reply += (
@@ -80,14 +90,23 @@ async def get_weather(message: types.Message):
                 f"Закат солнца: {sunset.time()}\n"
                 f"Продолжительность дня: {length_of_the_day}\n"
             )
-        message_reply += "Хорошего дня!"
-        await message.reply(message_reply)
-
+        message_reply += (
+            f"\nИндекс загрязнения воздуха: *{air_pollution_index}*\n _1 = Хорошее, 2 = Удовлетворительное, 3 = Умеренное, 4 = Плохое, 5 = Очень плохое._\n"
+            f"Содержание угарного газа: *{air_pollution_co}*\n"
+        )
+        message_reply += ("\nХорошего дня!\n")
+        
+        message_reply = message_reply.replace('-', r'\-')
+        message_reply = message_reply.replace('.', r'\.')
+        message_reply = message_reply.replace('!', r'\!')
+        message_reply = message_reply.replace('=', r'\=')
+        await message.reply(message_reply, parse_mode='MarkdownV2')
+        
     except requests.exceptions.RequestException as e:
         await message.reply("Ошибка при обращении к API погоды. Попробуйте позже.")
         print(f"Error: {e}")
     except KeyError:
-        await message.reply("Получен некорректный ответ от API погоды. Попробуйте позже. Возможно достигнут лимит запросов на день.")
+        await message.reply("Получен некорректный ответ от API погоды. Попробуйте позже.")
         print("Error: Invalid response from weather API.")
     except Exception as e:
         await message.reply("Произошла ошибка. Попробуйте позже.")
